452acfb147c20c703970f13a61012c06ab353991
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-22 14:20:25 +0530

    Add project files.

---
+++ DemoDssApiApp/Data/DemoDssApiAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoDssApiApp.Data
+{
+    public class DemoDssApiAppContext : DbContext
+    {
+        public DemoDssApiAppContext (DbContextOptions<DemoDssApiAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoDssApiApp.Data.Employee> Employee { get; set; }
+    }
+}
