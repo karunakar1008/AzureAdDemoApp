3ba37c68834225f547f538fdd2b67feec24286d0
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-22 15:09:43 +0530

    Add client app to API app to invoke apis.

---
+++ DemoClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoClientApp.Api;
+using DemoClientApp.Client;
+using System;
+
+namespace DemoClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configuration();
+            config.BasePath = "https://dssdemoapiapp345.azurewebsites.net/";
+            EmployeesApi api = new EmployeesApi(config);
+            var emps = api.ApiEmployeesGet();
+            foreach (var emp in emps)
+            {
+                Console.WriteLine(emp.Id + " " + emp.Name + " " + emp.Salary);
+            }
+        }
+    }
+}
